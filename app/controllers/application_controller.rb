7cf39615ab25ade3938da1ea7af0a1205f96d33d
Fion <fionpang97@gmail.com>
2019-11-03 14:15:53 -0800

    Setup user Auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+  #CRILL
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def require_login
+    unless current_user
+      render json: { base: ['invalid credentials'] }, status: 401
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login(user)
+    @current_user = user
+    session[:session_token] = @current_user.reset_session_token!
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
 end
