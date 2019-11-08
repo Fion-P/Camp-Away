248882471f136bc98639d5be7fc574b674c755a8
Fion <fionpang97@gmail.com>
2019-11-07 17:59:54 -0800

    Update store.js

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,12 +3,16 @@ import logger from "redux-logger";
 import thunk from "redux-thunk";
 import rootReducer from "../reducers/root_reducer";
 
-const configureStore = (preloadedState = {}) => (
-  createStore(
+const configureStore = (preloadedState = {}) => {
+  const middleware = [thunk];
+  if (process.env.NODE_ENV !== 'production') {
+    middleware = [...middleware, logger];
+  }
+   return createStore(
     rootReducer, 
     preloadedState, 
-    applyMiddleware(thunk, logger)
+    applyMiddleware(...middleware, logger)
   )
-);
+};
 
 export default configureStore;
