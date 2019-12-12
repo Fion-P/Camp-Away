c0b5ce7e5d6874bc2cd98d7a7fccc1555b595eca
Fion-P <fionpang97@gmail.com>
2019-12-12 10:43:01 -0800

    add info window for markers

--- frontend/util/marker_manager.js
+++ frontend/util/marker_manager.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 class MarkerManager {
   constructor(map, handleClick) {
     this.map = map;
@@ -6,16 +8,43 @@ class MarkerManager {
   }
 
   createMarkerFromCamp(camp) {
+
+    const markerInfoWindow = new google.maps.InfoWindow({
+      content:
+        `<div class="infowindow">
+          <img class="infowindow-image" src="${camp.photoUrls[0]}"/>
+          <div>
+            <h2 class="infowindow-title"> ${camp.name} </h2>
+            <p>${camp.location}</p>
+            <p>$${camp.price}/night</p>
+            </div>
+        </div>`,
+      maxWidth: 250,
+    });
+
     const position = new google.maps.LatLng(camp.latitude, camp.longitude);
     const marker = new google.maps.Marker({
       position,
       map: this.map,
       campId: camp.id,
-      icon: "smallpin.png"
+      icon: "smallpin.png",
+      title: camp.name,
+      infoWindow: markerInfoWindow,
+      // clicked: false
     });
 
     marker.addListener('click', () => this.handleClick(camp));
     this.markers[marker.campId] = marker;
+    // console.log(this.markers);
+
+    marker.addListener('mouseover', () => {
+      marker.infoWindow.open(this.map, marker);
+    });
+
+    marker.addListener('mouseout', () => {
+      if (!marker.clicked) marker.infoWindow.close(this.map, marker);
+    });
+
   }
 
   updateMarkers(camps) {
@@ -29,7 +58,7 @@ class MarkerManager {
     Object.keys(this.markers)
       .filter(campId => !campsObj[campId])
       .forEach((campId) => this.removeMarker(this.markers[campId]));
-      
+    
   }
   
 }
